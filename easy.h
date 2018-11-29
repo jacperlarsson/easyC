7b8cb00acf284825f7a9fde8a1cfab13bc4f2d20
jacper larsson <hamidou2hamidou@gmail.com>
2018-11-29 16:05:51 +0000

    Update easy.h

--- easy.h
+++ easy.h
@@ -1,11 +1,11 @@
-//if you want compare with two number and get the max
+//if you want compare with two number and get the max of two number
 #define MAX2(a,b) \
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
      
      
-     
+     //if you want compare with two number and get the minimum of two number
      #define Min2(a,b) \
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
